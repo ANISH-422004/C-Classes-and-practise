dc296f6efe7ef96efdb3106315933d2d70f59a13
ANISH-422004 <anishbhattacharyagtl@gmail.com>
2025-06-16 13:18:49 +0530

    Chapter1 Done

--- chapter1Practise/practise1.c
+++ chapter1Practise/practise1.c
@@ -9,18 +9,42 @@ int main(){
 
 
     // reactangle form user Input 
-    int a, b;
+    // int a, b;
 
-    // Ask the user for input
-    printf("Enter length: ");
-    scanf("%d", &a);  // store input into variable a
+    // printf("Enter length: ");
+    // scanf("%d", &a);  
 
-    printf("Enter breadth: ");
-    scanf("%d", &b);  // store input into variable b
+    // printf("Enter breadth: ");
+    // scanf("%d", &b); 
 
-    // Calculate and print area
-    int area = a * b;
-    printf("Area of rectangle is: %d\n", area);
+    // int area = a * b;
+    // printf("Area of rectangle is: %d\n", area);
+
+
+    // A program to convert Celsius to Fahrenheit formula ( F = C * 9/5 + 32 )
+    // float c, f;
+
+    // printf("celsius: ");
+    // scanf("%f" , &c);
+    // f = c * 9 / 5 + 32;
+    // printf("Fahrenheit: %f\n", f);
+
+
+
+
+    //Simple interest formula ( SI = P * R * T / 100 )
+    float p, r, t, si; 
+    printf("Enter principal amount: ");
+    scanf("%f", &p);
+    printf("Enter rate of interest: ");
+    scanf("%f", &r);
+    printf("Enter time in years: ");
+    scanf("%f", &t);
+    si = (p * r * t) / 100;
+    printf("Simple Interest: %f\n", si);
+
+    
+
 
     return 0;
 }
