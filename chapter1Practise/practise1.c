18db5a9df6d733eaad48556004a04e139cc6650a
ANISH-422004 <anishbhattacharyagtl@gmail.com>
2025-06-16 13:12:21 +0530

    scanf and printf

--- chapter1Practise/practise1.c
+++ chapter1Practise/practise1.c
@@ -3,10 +3,26 @@
 
 int main(){
     // rectangle hard coded
-    int l = 3;
-    float b = 2.5;
-    printf("The area of Rectangle %f \n " , l*b);
+    // int l = 3;
+    // float b = 2.5;
+    // printf("The area of Rectangle %f \n " , l*b);
 
+
+    // reactangle form user Input 
+    int a, b;
+
+    // Ask the user for input
+    printf("Enter length: ");
+    scanf("%d", &a);  // store input into variable a
+
+    printf("Enter breadth: ");
+    scanf("%d", &b);  // store input into variable b
+
+    // Calculate and print area
+    int area = a * b;
+    printf("Area of rectangle is: %d\n", area);
 
     return 0;
 }
+
+
