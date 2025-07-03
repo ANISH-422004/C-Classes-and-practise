2c78b9bc050c7047679872c67c04c47348485ec6
ANISH-422004 <anishbhattacharyagtl@gmail.com>
2025-07-03 22:01:25 +0530

    Statrd practising structures

--- PointerQuestionsPractise/1.c
+++ PointerQuestionsPractise/1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
     // poiter Type casting 
     int a = 10;
@@ -10,6 +10,43 @@ int main() {
     printf("Address of a: %p\n", (void *)&a);
     printf("Value of a using int pointer: %d\n", *p);
     printf("Value of a using float pointer (type casted): %f\n", *f);   
+
+
+    struct Student
+    {
+        int roll; 
+        char name[20];
+        float marks;
+    };
+    
+
+    // struct Student s1 = {1, "John Doe", 85.5};
+    // s1.roll = 2; // Changing roll number
+    // s1.marks = 90.0;
+    // // chaneing the name  to Anish
+    // strcopy(s1.name, "Anish"); // why so ?
+    // printf("Roll: %d, Name: %s, Marks: %.2f\n", s1.roll, s1.name, s1.marks);
+    
+
+    //creating array if struct student 
+    // struct Student students[3]; 
+
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     printf("Enter roll number: ");
+    //     scanf("%d", &students[i].roll);
+    //     printf("Enter name: ");
+    //     scanf("%s", &students[i].name);  
+    //     printf("Enter marks: ");
+    //     scanf("%f", &students[i].marks);
+
+    // }
+
+    // printf("\n===== Student Details =====\n");
+    // for (int i = 0; i < 2; i++) {
+    //     printf("Roll: %d, Name: %s, Marks: %.2f\n", students[i].roll, students[i].name, students[i].marks);
+    // }
+    
 
 
 
